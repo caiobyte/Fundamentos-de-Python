7cd9c85242647528723ac19d8ddac92826e2a2bc
Caio Azevedo <69327111+caiobyte@users.noreply.github.com>
2026-02-13 13:42:14 -0300

    Criação + Interação ; Calculator.py
    Criação de funções básicas de uma Calculadora;
    Fazer a Calculadora interativa com o input do usuário

---
+++ calculator.py
@@ -0,0 +1,35 @@
+def add(a, b):
+  return a + b
+
+def subtract(a, b):
+  return a - b
+
+def multiply(a, b):
+  return a * b
+
+def divide(a, b): 
+  if b == 0:
+    return "Cannot divide by zero"
+  return a / b
+
+print("Simple Calculator")
+print("1 - Add")
+print("2 - Subtract")
+print("3 - Multiply")
+print("4 - Divide")
+
+choice = input("Choose an option: ")
+
+a = float(input("First number: "))
+b = float(input("Second number: "))
+
+if choice == "1":
+  print("Result: ", add(a, b))
+elif choice == "2":
+  print("Result: ", subtract(a, b))
+elif choice == "3":
+  print("Result: ", multiply(a, b))
+elif choice == "4":
+  print("Result: ", divide(a, b))
+else:
+  print("Invalid Operation")
